632e7f375319cc4f99afdcb58043ba53ab9e5b0d
zhaojianhao <498763852@qq.com>
2017-11-15 20:01:00 +0800

    商品

--- app/application/comm/controller/Index.php
+++ app/application/comm/controller/Index.php
@@ -1,5 +1,5 @@
 <?php
-namespace app\comm\controller;
+namespace app\index\controller;
 use think\View;
 use think\Db;
 use think\Controller;
@@ -49,11 +49,23 @@ class Index extends Controller
     public function get_goods()
     {    
            //接收信息
-    	 $goods_id=isset($_GET['goods_id'])?$_GET['goods_id']:"";
+    	      $goods_id=isset($_GET['goods_id'])?$_GET['goods_id']:"";
+           $user_id=isset($_GET['user_id'])?$_GET['user_id']:"";
+             //查询是否收藏
+           $favorite=db("favorite")->where("rid={$goods_id} and user_id={$user_id}")->find();
+           if(!empty($favorite))
+           {           
+               $favorite=0;
+           }
+           else
+           {
+               $favorite=1;
+           }   
            //商品信息
-    	 $goods=db("goods")->where("id='{$goods_id}'")->find();
+       	 $goods=db("goods")->where("id='{$goods_id}'")->find();
            $imgs=db("goods_photo_relation as r")->join("goods_photo p","r.photo_id=p.id","LEFT")->limit(3)->where("goods_id='{$goods_id}'")->select();
            $goods['imgs']=$imgs;
+           $goods['favorite']=$favorite;
            if($imgs)
            {
                $this->message("0",$goods);
@@ -73,6 +85,34 @@ class Index extends Controller
         //货品信息
         $products=db("products")->where("goods_id='{$goods_id}' and spec_array='{$specJSON}'")->find();
         $this->message("0",$products);
+    }
+    //收藏
+    public function get_favorite()
+    {
+        //接收信息
+        $data['rid']=isset($_GET['goods_id'])?$_GET['goods_id']:"";
+        $data['user_id']=isset($_GET['user_id'])?$_GET['user_id']:"";
+        //查询是否收藏
+        $favorite=db("favorite")->where("rid='{$data['rid']}' and user_id='{$data['user_id']}'")->find();
+        if($favorite)
+        {
+            //取消收藏
+            db("favorite")->where("id='{$favorite['id']}'")->delete();
+            $this->message("0",1);     
+        }
+        else
+        {
+           $data['time']=date("Y-m-d H:i:s",time());
+           $data['summary']="";
+           //分类id
+           $category=db("category_extend")->where("id='{$data['rid']}'")->find();
+           $category['category_id']=isset($category['category_id'])?$category['category_id']:"";
+           $data['cat_id']=$category['category_id'];
+           //收藏成功
+           db("favorite")->insert($data);  
+           $this->message("0",0);   
+        }
+                
 
     }
 }
